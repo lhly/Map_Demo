6522ab9b095f81aa81fa3a0fa3ab6a6bed098311
lhly <lhlyzh@outlook.com>
2018-12-26 22:02:56 +0800

    update

--- app/src/main/java/cn/lhlyblog/map_demo/MainActivity.java
+++ app/src/main/java/cn/lhlyblog/map_demo/MainActivity.java
@@ -1,6 +1,5 @@
 package cn.lhlyblog.map_demo;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,7 +9,6 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.amap.api.location.AMapLocation;
@@ -18,33 +16,16 @@ import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
-import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.SupportMapFragment;
-import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.LatLngBounds;
 import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
-import com.amap.api.services.core.LatLonPoint;
-import com.amap.api.services.core.PoiItem;
-import com.amap.api.services.core.SuggestionCity;
-import com.amap.api.services.help.Tip;
-import com.amap.api.services.poisearch.PoiResult;
-import com.amap.api.services.poisearch.PoiSearch;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import cn.lhlyblog.map_demo.overlay.PoiOverlay;
-import cn.lhlyblog.map_demo.util.Constants;
-import cn.lhlyblog.map_demo.util.ToastUtil;
 
 public class MainActivity extends CheckPermissions
-        implements LocationSource, AMapLocationListener,
-        AMap.OnMarkerClickListener, AMap.InfoWindowAdapter,
-        PoiSearch.OnPoiSearchListener, View.OnClickListener {
+        implements LocationSource, AMapLocationListener {
 
     private WifiManager mWifiManager;
     private TextView mLocationErrText;
@@ -52,24 +33,11 @@ public class MainActivity extends CheckPermissions
     private AMap mMap;
     private SensorEventHelper sensorEventhelper;
     private Marker marker;
-    private String mKeyWords = "";// 要输入的poi搜索关键字
-    private ProgressDialog progDialog = null;// 搜索时进度条
-
-    private PoiResult poiResult; // poi返回的结果
-    private int currentPage = 1;
-    private PoiSearch.Query query;// Poi查询条件类
-    private PoiSearch poiSearch;// POI搜索
-    private TextView mKeywordsTextView;
-    private Marker mPoiMarker;
-    private ImageView mCleanKeyWords;
 
     private LocationSource.OnLocationChangedListener mListener;
     private AMapLocationClient mlocationClient;
     private AMapLocationClientOption mLocationOption;
 
-    public static final int REQUEST_CODE = 100;
-    public static final int RESULT_CODE_INPUTTIPS = 101;
-    public static final int RESULT_CODE_KEYWORDS = 102;
     LatLng southwestLatLng = new LatLng(34.62317, 112.597139);
     LatLng northeastLatLng = new LatLng(34.63627, 112.613232);
     LatLngBounds latLngBounds = new LatLngBounds.Builder()
@@ -82,11 +50,8 @@ public class MainActivity extends CheckPermissions
         mLocationErrText = (TextView) findViewById(R.id.location_errInfo_text);
         mWifiManager = (WifiManager) this.getApplicationContext()
                 .getSystemService(Context.WIFI_SERVICE);
-        mCleanKeyWords = (ImageView)findViewById(R.id.clean_keywords);
-        mCleanKeyWords.setOnClickListener(this);
 
         init();
-        mKeyWords = "";
     }
 
     private void init() {
@@ -95,8 +60,6 @@ public class MainActivity extends CheckPermissions
         if (sensorEventhelper != null) {
             sensorEventhelper.registerSensorListener();
         }
-        mKeywordsTextView = (TextView) findViewById(R.id.main_keywords);
-        mKeywordsTextView.setOnClickListener(this);
     }
 
     private void setUpMap() {
@@ -110,8 +73,6 @@ public class MainActivity extends CheckPermissions
         mMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
         // 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
         mMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
-        mMap.setOnMarkerClickListener(this);// 添加点击marker监听事件
-        mMap.setInfoWindowAdapter(this);// 添加显示infowindow监听事件
     }
 
     private void setUpMapIfNeeded() {
@@ -250,211 +211,5 @@ public class MainActivity extends CheckPermissions
             mlocationClient.onDestroy();
         }
         mlocationClient = null;
-    }
-
-    /**
-     * 显示进度框
-     */
-    private void showProgressDialog() {
-        if (progDialog == null)
-            progDialog = new ProgressDialog(this);
-        progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progDialog.setIndeterminate(false);
-        progDialog.setCancelable(false);
-        progDialog.setMessage("正在搜索:\n" + mKeyWords);
-        progDialog.show();
-    }
-
-    /**
-     * 隐藏进度框
-     */
-    private void dissmissProgressDialog() {
-        if (progDialog != null) {
-            progDialog.dismiss();
-        }
-    }
-
-    /**
-     * 开始进行poi搜索
-     */
-    protected void doSearchQuery(String keywords) {
-        List<LatLonPoint> points = new ArrayList<LatLonPoint>();
-        points.add(new LatLonPoint(34.635265, 112.597311));
-        points.add(new LatLonPoint(34.636361, 112.612717));
-        points.add(new LatLonPoint(34.624495, 112.612889));
-        points.add(new LatLonPoint(34.623524, 112.597267));
-
-        showProgressDialog();// 显示进度框
-        currentPage = 1;
-        // 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
-        query = new PoiSearch.Query(keywords, "");
-        // 设置每页最多返回多少条poiitem
-        query.setPageSize(10);
-        // 设置查第一页
-        query.setPageNum(currentPage);
-
-        poiSearch = new PoiSearch(this, query);
-        poiSearch.setBound(new PoiSearch.SearchBound(points));
-        poiSearch.setOnPoiSearchListener(this);
-        poiSearch.searchPOIAsyn();
-    }
-
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        marker.showInfoWindow();
-        return false;
-    }
-
-    @Override
-    public View getInfoContents(Marker marker) {
-        return null;
-    }
-
-    @Override
-    public View getInfoWindow(final Marker marker) {
-        View view = getLayoutInflater().inflate(R.layout.poikeywordsearch_uri,
-                null);
-        TextView title = (TextView) view.findViewById(R.id.title);
-        title.setText(marker.getTitle());
-
-        TextView snippet = (TextView) view.findViewById(R.id.snippet);
-        snippet.setText(marker.getSnippet());
-        return view;
-    }
-
-    /**
-     * poi没有搜索到数据，返回一些推荐城市的信息
-     */
-    private void showSuggestCity(List<SuggestionCity> cities) {
-        String infomation = "推荐城市\n";
-        for (int i = 0; i < cities.size(); i++) {
-            infomation += "城市名称:" + cities.get(i).getCityName() + "城市区号:"
-                    + cities.get(i).getCityCode() + "城市编码:"
-                    + cities.get(i).getAdCode() + "\n";
-        }
-        ToastUtil.show(MainActivity.this, infomation);
-
-    }
-
-
-    /**
-     * POI信息查询回调方法
-     */
-    @Override
-    public void onPoiSearched(PoiResult result, int rCode) {
-        dissmissProgressDialog();// 隐藏对话框
-        if (rCode == 1000) {
-            if (result != null && result.getQuery() != null) {// 搜索poi的结果
-                if (result.getQuery().equals(query)) {// 是否是同一条
-                    poiResult = result;
-                    // 取得搜索到的poiitems有多少页
-                    List<PoiItem> poiItems = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
-                    List<SuggestionCity> suggestionCities = poiResult
-                            .getSearchSuggestionCitys();// 当搜索不到poiitem数据时，会返回含有搜索关键字的城市信息
-
-                    if (poiItems != null && poiItems.size() > 0) {
-                        mMap.clear();// 清理之前的图标
-                        PoiOverlay poiOverlay = new PoiOverlay(mMap, poiItems);
-                        poiOverlay.removeFromMap();
-                        poiOverlay.addToMap();
-                        poiOverlay.zoomToSpan();
-                    } else if (suggestionCities != null
-                            && suggestionCities.size() > 0) {
-                        showSuggestCity(suggestionCities);
-                    } else {
-                        ToastUtil.show(MainActivity.this,
-                                R.string.no_result);
-                    }
-                }
-            } else {
-                ToastUtil.show(MainActivity.this,
-                        R.string.no_result);
-            }
-        } else {
-            ToastUtil.showerror(this, rCode);
-        }
-
-    }
-
-    @Override
-    public void onPoiItemSearched(PoiItem item, int rCode) {
-        // TODO Auto-generated method stub
-
-    }
-
-    /**
-     * 输入提示activity选择结果后的处理逻辑
-     *
-     * @param requestCode
-     * @param resultCode
-     * @param data
-     */
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_CODE_INPUTTIPS && data
-                != null) {
-            mMap.clear();
-            Tip tip = data.getParcelableExtra(Constants.EXTRA_TIP);
-            if (tip.getPoiID() == null || tip.getPoiID().equals("")) {
-                doSearchQuery(tip.getName());
-            } else {
-                addTipMarker(tip);
-            }
-            mKeywordsTextView.setText(tip.getName());
-            if(!tip.getName().equals("")){
-                mCleanKeyWords.setVisibility(View.VISIBLE);
-            }
-        } else if (resultCode == RESULT_CODE_KEYWORDS && data != null) {
-            mMap.clear();
-            String keywords = data.getStringExtra(Constants.KEY_WORDS_NAME);
-            if(keywords != null && !keywords.equals("")){
-                doSearchQuery(keywords);
-            }
-            mKeywordsTextView.setText(keywords);
-            if(!keywords.equals("")){
-                mCleanKeyWords.setVisibility(View.VISIBLE);
-            }
-        }
-    }
-
-    /**
-     * 用marker展示输入提示list选中数据
-     *
-     * @param tip
-     */
-    private void addTipMarker(Tip tip) {
-        if (tip == null) {
-            return;
-        }
-        mPoiMarker = mMap.addMarker(new MarkerOptions());
-        LatLonPoint point = tip.getPoint();
-        if (point != null) {
-            LatLng markerPosition = new LatLng(point.getLatitude(), point.getLongitude());
-            mPoiMarker.setPosition(markerPosition);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, 17));
-        }
-        mPoiMarker.setTitle(tip.getName());
-        mPoiMarker.setSnippet(tip.getAddress());
-    }
-
-    /**
-     * 点击事件回调方法
-     */
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.main_keywords:
-                Intent intent = new Intent(this, InputTipsActivity.class);
-                startActivityForResult(intent, REQUEST_CODE);
-                break;
-            case R.id.clean_keywords:
-                mKeywordsTextView.setText("");
-                mMap.clear();
-                mCleanKeyWords.setVisibility(View.GONE);
-            default:
-                break;
-        }
     }
 }
